976838176c3da9f780364f61658b613749bfaa61
Rohit Dutta <rohitdutta2.21@nshm.edu.in>
2025-01-23 22:59:27 +0530

    express_dir updated with part-1

--- express_dir/index.js
+++ express_dir/index.js
@@ -7,6 +7,50 @@ app.listen(port, ()=> {
     console.log(`app listening to port: "${port}"`);
 });
 
-app.use((req, res) => {
-    console.log("request received!");
+app.get("/", (req, res)=> {
+    res.send("you entered the root route!")
 })
+
+// path-parameters
+app.get("/:username/:id", (req, res)=> {
+    let {username, id} = req.params;
+    let htmlStr = `Welcome to the page of @${username}`
+    res.send(htmlStr);
+})
+
+// query string
+app.get("/search", (req, res)=> {
+    let {q} = req.query;
+    if(!q){
+        res.send(`<h1>nothing searched</h1>`);
+    }
+    else
+        res.send(`<h1>you searched for: ${q}</h1>`);
+});
+
+app.get("/apple", (req, res)=> {
+    res.send("you entered the apple path!")
+})
+
+app.get("/orange", (req, res)=> {
+    res.send("you entered the orange path!")
+})
+
+// standard response if path or route doesn't exist but user entered to search
+app.get("*", (req, res)=> {
+    res.send("this route doesn't exist.")
+})
+
+app.post("/new", (req, res)=> {
+    res.send("this is a post request.")
+})
+
+// app.use((req, res) => {
+//     // console.log(req); // prints the response object
+//     console.log("request received!");
+//     // res.send("Hello! nice to meet you </br> <h1>This is a Header</h1>");
+//     res.send({
+//         name: "rohit",
+//         role: "developer"
+//     });
+// })
